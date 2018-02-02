7fecbf4907fae260a7e8bba4f88aa5203fb1f98a
Terry <tgaitan@csumb.edu>
2018-02-02 00:06:07 +0000

    initial commit added composer json and assignments

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+<div>
+    <h1> LAB SECTION</h1>
+    <a href= "/labs/1">Lab 1</a>
+</div>
+    </body>
+</html>
---
+++ labs/1/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>
+            LAB 1
+        </title>
+    </head>
+    <body>
+        This is lab 1
+        
+        <div><a href="/">Back</a></div>
+    </body>
+</html>
